feca7fcdd608b29b24fb3a37d3db602c52d0e83a
MickMake <github@mickmake.com>
2020-02-17 13:04:56 +1100

    Latest push

--- bin/_GetVersions.sh
+++ bin/_GetVersions.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 
+# WARNING: This file is SOURCED. Don't add in any "exit", otherwise your shell will exit.
+
 getall() {
 	VERSIONS="$(find * -maxdepth 1 -type f -name 'gearbox.json' | sed 's/\/gearbox.json//')"
 }
 
-if [ "$1" == "all" ]
+
+################################################################################
+DIR="$(dirname $0)"
+
+GB_GITURL="$(git config --get remote.origin.url)"; export GB_GITURL
+GB_GITREPO="$(basename -s .git ${GB_GITURL})"; export GB_GITREPO
+
+if [ "${GB_GITREPO}" == "docker-template" ]
 then
-	getall
+	echo "Cannot run this command from the docker-template repository."
+	echo "IF THIS IS A COPY of that repo, then..."
+	echo "	1. Remove the .git directory."
+	echo "	2. Run the \"make init\" command."
+	echo ""
+	unset VERSIONS GB_GITURL GB_GITREPO
+
 else
-	VERSIONS="$@"
-fi
+	if [ "$1" == "all" ]
+	then
+		getall
+	else
+		VERSIONS="$@"
+	fi
 
-if [ "${VERSIONS}" == "" ]
-then
-	echo "# Gearbox: ERROR - No versions specified."
-	getall
-	echo "# Gearbox: Versions available - ${VERSIONS}"
-	echo "	all - All versions"
-	for version in ${VERSIONS}
-	do
-		echo "	$(./bin/JsonToConfig-Darwin -json "${version}/gearbox.json" -template-string '{{ .Json.version }} - {{ .Json.organization }}/{{ .Json.name }}:{{ .Json.version }}')"
-	done
-	unset VERSIONS
+	if [ "${VERSIONS}" == "" ]
+	then
+		echo "# Gearbox: ERROR - No versions specified."
+		getall
+		echo "# Gearbox: Versions available - ${VERSIONS}"
+		echo "	all - All versions"
+		for version in ${VERSIONS}
+		do
+			echo "	$("${DIR}/JsonToConfig-Darwin" -json "${version}/gearbox.json" -template-string '{{ .Json.version }} - {{ .Json.organization }}/{{ .Json.name }}:{{ .Json.version }}')"
+		done
+		unset VERSIONS
+	fi
 fi
-
--- bin/release.sh
+++ bin/release.sh
@@ -9,30 +9,42 @@ then
 	echo "# Gearbox: Running ${0} failed"
 	exit 1
 fi
-echo "# Gearbox: Running ${0} for versions: ${VERSIONS}"
+echo "# Gearbox: Running ${0} for repository containing versions: ${VERSIONS}"
 
-for VERSION in "${VERSIONS}"
-do
-	JSONFILE="${VERSION}/gearbox.json"
-	if [ ! -f "${JSONFILE}" ]
-	then
-		echo "Gearbox: Can't find JSON file: ${JSONFILE}"
-		exit
-	fi
 
-	${DIR}/_GetEnv.sh "${JSONFILE}"
-	. "${VERSION}/.env"
+"${DIR}/push-github.sh" "$@"
 
-	echo "# Gearbox[${GB_IMAGEMAJORVERSION}]: Not implemented yet."
 
-#	echo "# Gearbox[${GB_IMAGEMAJORVERSION}]: List image."
-#	docker image ls ${GB_IMAGEMAJORVERSION}
-#	echo "# Gearbox[${GB_IMAGEVERSION}]: List image."
-#	docker image ls ${GB_IMAGEVERSION}
-#
-#	echo "# Gearbox[${GB_CONTAINERMAJORVERSION}]: List container."
-#	docker container ls -f name="^${GB_CONTAINERMAJORVERSION}"
-#	echo "# Gearbox[${GB_CONTAINERVERSION}]: List container."
-#	docker container ls -f name="^${GB_CONTAINERVERSION}"
-done
+echo "# Gearbox[${GB_GITREPO}]: Creating release on GitHub."
+if [ "${GB_GITREPO}" == "" ]
+then
+	echo "# Gearbox[${GB_GITREPO}]: GB_GITREPO needs to be set to create a release."
+	echo "# Gearbox[${GB_GITREPO}]: Have you run \"git init\"?"
+	PUSHRELEASE="Y"
+fi
+
+if [ "${GITHUB_USER}" == "" ]
+then
+	echo "# Gearbox[${GB_GITREPO}]: GITHUB_USER needs to be set to create a release."
+	PUSHRELEASE="Y"
+fi
+
+if [ "${GITHUB_TOKEN}" == "" ]
+then
+	echo "# Gearbox[${GB_GITREPO}]: GITHUB_TOKEN needs to be set to create a release."
+	PUSHRELEASE="Y"
+fi
+
+
+if [ "${PUSHRELEASE}" == "" ]
+then
+	echo "# Gearbox[${GB_GITREPO}]: Abandoning GitHub release creation."
+else
+	echo "# Gearbox[${GB_GITREPO}]: Uploading release to GitHub."
+	tar zcvf release.tgz Makefile README.md bin
+	${DIR}/github-release upload --user "gearboxworks" --repo "${GB_GITREPO}" --tag 1.0.0 --name "release.tgz" --label "release.tgz" -R -f release.tgz
+fi
+
+
+"${DIR}/push-dockerhub.sh" "$@"
 
